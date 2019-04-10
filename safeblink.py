a9ad846ca8c22b862d5e9cb28423f4a19cd65abf
ssreekanth2000 <s.sreekanth2000@gmail.com>
2019-04-09 22:50:54 -0400

    blink

---
+++ safeblink.py
@@ -0,0 +1,44 @@
+
+import time
+import RPi.GPIO as GPIO
+dot = [1, 0]
+dash = [1, 1, 1, 0]
+let_brk = [0]*2
+wor_brk = [0]*4
+letter_dict = {"a": dot + dash,
+                "b": dash + dot + dot + dot,
+                "c": dash + dot + dash + dot,
+                "d": dash + dot + dot,
+                "e": dot,
+                "f": dot + dot + dash + dot,
+                "g": dash + dash + dot,
+                "h": dot + dot + dot + dot,
+                "i": dot + dot,
+                "j": dot + dash + dash + dash,
+                "k": dash + dot + dash,
+                "l": dot + dash + dot + dot,
+                "m": dash + dash,
+                "n": dash + dot,
+                "o": dash + dash + dash,
+                "p": dot + dash + dash + dot,
+                "q": dash + dash + dot + dash,
+                "r": dot + dash + dot,
+                "s": dot + dot + dot,
+                "t": dash,
+                "u": dot + dot + dash,
+                "v": dot + dot + dot + dash,
+                "w": dot + dash + dash,
+                "x": dash + dot + dot + dash,
+                "y": dash + dot + dash + dash,
+                "z": dash + dash + dot + dot,
+                "1": dot + dash + dash + dash + dash,
+                "2": dot + dot + dash + dash + dash,
+                "3": dot + dot + dot + dash + dash,
+                "4": dot + dot + dot + dot + dash,
+                "5": dot + dot + dot + dot + dot,
+                "6": dash + dot + dot + dot + dot,
+                "7": dash + dash + dot + dot + dot,
+                "8": dash + dash + dash + dot + dot,
+                "9": dash + dash + dash + dash + dot,
+                "0": dash + dash + dash + dash + dash,
+                " ": wor_brk}

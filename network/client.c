ff230ff810cedb1167e27833c73be5c4fdf66be1
Kawin Nikomborirak <concavemail@gmail.com>
2019-04-15 21:55:53 +0000

    Implement multiple clients

--- network/client.c
+++ network/client.c
@@ -4,38 +4,19 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
-#include <unistd.h>
-#define PORT 8080
+
 int main() {
-  struct sockaddr_in_address;
-  int sock = 0, valread;
-  struct sockaddr_in serv_addr;
-  char *hello = "Hello from client";
-  char buffer[1024] = {0};
-  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-    printf("\n Socketcreation error \n");
-    return -1;
+  int fd = socket(AF_INET, SOCK_STREAM, 0);
+  struct sockaddr_in serv;
+  serv.sin_family = AF_INET;
+  serv.sin_port = htons(8096);
+  inet_pton(AF_INET, "127.0.0.1", &serv.sin_addr);
+  connect(fd, (struct sockaddr *)&serv, sizeof serv);
+  char message[100] = "";
+
+  while (1) {
+    printf("Enter a message: ");
+    fgets(message, 100, stdin);
+    send(fd, message, strlen(message), 0);
   }
-
-  memset(&serv_addr, '0', sizeof(serv_addr));
-
-  serv_addr.sin_family = AF_INET;
-  serv_addr.sin_port = htons(PORT);
-
-  if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
-    printf("\nInvalid address/ Address not supported \n");
-    return -1;
-  }
-
-  if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-    printf("\nConnection Failed\n");
-    return -1;
-  }
-
-  send(sock, hello, strlen(hello), 0);
-  printf("Hello Message Sent\n");
-
-  valread = read(sock, buffer, 1024);
-  printf("%s\n", buffer);
-  return 0;
 }

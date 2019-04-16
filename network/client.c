247d241256103ebc781c3032abe8ae5e7f87fd92
Kawin Nikomborirak <concavemail@gmail.com>
2019-04-16 15:35:11 +0000

    Minor refactor

--- network/client.c
+++ network/client.c
@@ -5,7 +5,9 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+
 #define PORT 8080
+
 int main() {
   struct sockaddr_in_address;
   int sock = 0, valread;
@@ -13,11 +15,11 @@ int main() {
   char *hello = "Hello from client";
   char buffer[1024] = {0};
   if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-    printf("\n Socketcreation error \n");
+    printf("\n Socket creation error \n");
     return -1;
   }
 
-  memset(&serv_addr, '0', sizeof(serv_addr));
+  memset(&serv_addr, '0', sizeof serv_addr);
 
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_port = htons(PORT);
@@ -27,7 +29,7 @@ int main() {
     return -1;
   }
 
-  if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+  if (connect(sock, (struct sockaddr *)&serv_addr, sizeof serv_addr) < 0) {
     printf("\nConnection Failed\n");
     return -1;
   }
--- network/server.c
+++ network/server.c
@@ -11,9 +11,9 @@ int main() {
   int server_fd, new_socket, valread;
   struct sockaddr_in address;
   int opt = 1;
-  int addrlen = sizeof(address);
-  char buffer[1024] = {0};
-  char *hello = "Hello server";
+  int addrlen = sizeof address;
+  char buffer[1024] = "";
+  char *hello = "Hello from server";
 
   if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
     perror("socket failed");
@@ -21,17 +21,16 @@ int main() {
   }
 
   if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt,
-                 sizeof(opt))) {
-    perror("setsockopt");
-    exit(EXIT_FAILURE);
+                 sizeof opt)) {
   }
 
   address.sin_family = AF_INET;
   address.sin_addr.s_addr = INADDR_ANY;
   address.sin_port = htons(PORT);
+  address.sin_port = htons(PORT);
 
-  if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-    perror("bind failed");
+  if (bind(server_fd, (struct sockaddr *)&address, sizeof address) < 0) {
+    perror("bind filed");
     exit(EXIT_FAILURE);
   }
 
@@ -49,6 +48,6 @@ int main() {
   valread = read(new_socket, buffer, 1024);
   printf("%s\n", buffer);
   send(new_socket, hello, strlen(hello), 0);
-  printf("Hello message sent\n");
+  puts("Hello Message sent.");
   return 0;
 }

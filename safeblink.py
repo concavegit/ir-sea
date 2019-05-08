d3275c4a8ef50e7eee761faee845a83c558bd553
ssreekanth2000 <s.sreekanth2000@gmail.com>
2019-05-08 19:22:21 -0400

    sad

--- safeblink.py
+++ safeblink.py
@@ -3,45 +3,12 @@ import time
 import RPi.GPIO as GPIO
 dot = [1, 0]
 dash = [1, 1, 1, 0]
-let_brk = [0]*2
+
 wor_brk = [0]*4
-letter_dict = {"a": dot + dash,
-                "b": dash + dot + dot + dot,
-                "c": dash + dot + dash + dot,
-                "d": dash + dot + dot,
-                "e": dot,
-                "f": dot + dot + dash + dot,
-                "g": dash + dash + dot,
-                "h": dot + dot + dot + dot,
-                "i": dot + dot,
-                "j": dot + dash + dash + dash,
-                "k": dash + dot + dash,
-                "l": dot + dash + dot + dot,
-                "m": dash + dash,
-                "n": dash + dot,
-                "o": dash + dash + dash,
-                "p": dot + dash + dash + dot,
-                "q": dash + dash + dot + dash,
-                "r": dot + dash + dot,
-                "s": dot + dot + dot,
-                "t": dash,
-                "u": dot + dot + dash,
-                "v": dot + dot + dot + dash,
-                "w": dot + dash + dash,
-                "x": dash + dot + dot + dash,
-                "y": dash + dot + dash + dash,
-                "z": dash + dash + dot + dot,
-                "1": dot + dash + dash + dash + dash,
-                "2": dot + dot + dash + dash + dash,
-                "3": dot + dot + dot + dash + dash,
-                "4": dot + dot + dot + dot + dash,
-                "5": dot + dot + dot + dot + dot,
-                "6": dash + dot + dot + dot + dot,
-                "7": dash + dash + dot + dot + dot,
-                "8": dash + dash + dash + dot + dot,
-                "9": dash + dash + dash + dash + dot,
-                "0": dash + dash + dash + dash + dash,
-                " ": wor_brk}
+letter_dict = {"1": dash,
+               "0": dot.# 1 is a dot, 0 is a dash, the variation is in time. There will be a
+               "  ": wor_brk}
+
 
 def letter_to_morse(letter):
 
@@ -52,7 +19,7 @@ def sequence_to_morse(sequence):
     for letter in sequence:
         sequence_in_morse += letter_to_morse(letter)
     return sequence_in_morse[:-2]
-s=sequence_to_morse("why why why pi pi pi pip ip ip i pi p i pi pi p ip ip ipipi pi pi pi pi pi pi pi pi pip ip ip ip ip ip i pi pi pi pi pi pi i   jhj hj hj hj hj jhjhcjch ")
+s=sequence_to_morse("1110 1100 1110 1000 1100 1100 1000 1000  ")
 
 if __name__ == '__main__':
     pass
